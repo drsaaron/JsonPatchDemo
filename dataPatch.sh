82a0080f71f17eafe56baf3e7792d97a8e0d6870
AARON <scottaaron@northwesternmutual.com>
2024-04-17 14:35:56 -0500

    ,

--- dataPatch.sh
+++ dataPatch.sh
@@ -1,19 +1,21 @@
 #! /bin/sh
 
+urlRoot=http://localhost:21000
+
 echo "getting all data...."
-curl http://localhost:21000/person
+curl $urlRoot/person
 
 echo
 echo "add new person...."
-curl -X POST -H "Content-Type: application/json" http://localhost:21000/person -d '{ "name": "Charles", "birthDate": "1950-04-15" }'
+curl -X POST -H "Content-Type: application/json" $urlRoot/person -d '{ "name": "Charles", "birthDate": "1950-04-15" }'
 
 echo
 echo "killing off person #2"
-curl -X PATCH -H "Content-Type: application/json-patch+json" http://localhost:21000/person/2 -d '[ { "op": "replace", "path": "/deathDate", "value": "1600-02-15" } ]'
+curl -X PATCH -H "Content-Type: application/json-patch+json" $urlRoot/person/2 -d '[ { "op": "replace", "path": "/deathDate", "value": "1600-02-15" } ]'
 
 echo
 echo "getting all people again"
-curl http://localhost:21000/person
+curl $urlRoot/person
 
 echo
 
